0f3d802b163502ede8b2fb7ccbc958a39f28985b
janimaenpaa <itsmayd@gmail.com>
2020-12-19 00:34:28 +0200

    Add cors

--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,14 @@
 import express from "express"
+import cors from "cors"
 import names from "./data/names"
 import { sortByPopularity } from "./utils"
 
-const app = express()
+const app = express().use(cors())
 
 const PORT = 3001
 
 app.get("/api/names", (_req, res) => {
+  console.log("Names")
   const sortedData = sortByPopularity(names)
   res.send(sortedData)
 })
